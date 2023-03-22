5c7a75e2c4485518e9df2f70514da6dae8fbd009
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2023-03-22 17:49:38 +0700

    Add project files.

---
+++ latihan2_looping/latihan2_looping.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[5];
+	cout << "Mengisi Array" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "data ke- " << (i + 1) << ": ";
+		cin >> a[i];
+	}
+	cout << "Membaca Array" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "data ke-" << (i + 1) << a[i] << endl;
+	}
+}
